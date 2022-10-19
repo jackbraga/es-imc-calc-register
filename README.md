934dd10656303fc8c65537504a7d49ce19c33515
jonatasandradedeveloper <jonatasandrade.develop@gmail.com>
2022-10-19 20:39:38 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+<h3>André Yamachi RA: 2201870</h3>
+<h3>Daniel Valle RA: 1600204</h3>
+<h3>Jackson Santos RA:2200577</h3>
+<h3>Jonatas Andrade RA:2201911</h3>
+<h3>Eduardo Luiz RA:2200568</h3>
+<h3>Pricilla Souza RA:2200555</h3>
+<h3>Clarindo Sal RA:2200105</h3>
+<h3>William Nunes RA:2200274</h3>
+<h3>Lucas Martins RA: 2201526</h3>
+<h3>Flavia Vastres RA: 2200463</h3>
---
+++ components/register/Register.form.tsx
@@ -0,0 +1,107 @@
+import React from "react"
+import { Dimensions, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+
+export default function RegisterForm(props: any) {
+    return <View style={styles.container}>
+        <View style={styles.fields}>
+            <Text style={styles.accountTitle}>
+                    Cadastro
+                </Text>
+            <TextInput
+                style={styles.input}
+                placeholder='Digite seu email'
+                autoCapitalize="none"
+                allowFontScaling={true}
+                secureTextEntry={false} />
+            <TextInput
+                style={styles.input}
+                placeholder='Digite sua senha'
+                autoCapitalize="none"
+                allowFontScaling={true}
+                secureTextEntry={true} />
+            
+            <View style={styles.filler}></View>
+        </View>
+        <View style={styles.actions}>
+            <TouchableOpacity
+                style={styles.btnRegister}
+                onPress={() => props.navigation.navigate("Home")}
+            >
+                <Text style={styles.btnRegisterText}>Cadastrar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.createAccountLink}
+                onPress={() => props.navigation.navigate("Login")}
+            >
+                <Text style={styles.accountActionText}>
+                    Já tenho uma conta!
+                </Text>
+            </TouchableOpacity>
+        </View>
+    </View>
+}
+
+const styles = StyleSheet.create({
+    container: {
+        paddingLeft: "5%",
+        paddingRight: "5%",
+        display: 'flex',
+        flex: 10,
+        flexDirection: 'column',
+        alignContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    fields: {
+        display: 'flex',
+        flex: 5,
+        flexDirection: 'column',
+        justifyContent: 'center'
+    },
+    input: {
+        borderBottomWidth: 1,
+        borderBottonColor: '#aaa',
+        width: Dimensions.get('screen').width * 0.9,
+        flex: 1.5
+    },
+    filler: {
+        flex: 1,
+    },
+    forgetPasswordLink: {
+        flex: 1,
+        marginTop: 5,
+    },
+    createAccountLink: {
+        flex: 1,
+        marginTop: 5,
+    },
+    accountTitle: {
+      color: "#000",
+      textAlign: "center",
+        marginTop: 20,
+        fontSize: 24
+    },
+    accountActionText: {
+        color: "#00A6FF",
+        textAlign: "center",
+        marginTop: 20
+    },
+    actions: {
+        flex: 2,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start"
+    },
+    btnRegister: {
+        backgroundColor: "#42B0BF",
+        width: Dimensions.get('screen').width * 0.9,
+        height: Dimensions.get('screen').height * 0.08,
+        alignContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    btnRegisterText: {
+        color: "#fff",
+        fontSize: 16
+    }
+});
